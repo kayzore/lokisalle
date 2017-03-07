76c5abfea75d43d66e5d1947fdffc79fc237bf12
jerome-lelievre <jerome.lelievre9@gmail.com>
2017-03-07 12:46:24 +0100

    adaptation de l'autoloader

--- app/autoload.php
+++ app/autoload.php
@@ -1,6 +1,8 @@
 <?php
 spl_autoload_register(function ($classname){
     $file = __DIR__ . '/../' . str_replace('\\', '/', $classname) . '.php';
-
+    if (!file_exists($file)) {
+        $file = '../src/' . str_replace('\\', '/', $classname) . '.php';
+    }
     include_once $file;
 });
