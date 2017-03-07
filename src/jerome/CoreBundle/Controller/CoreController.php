dcdf2591aa89461197d819dbe8388ca59cc10177
jerome-lelievre <jerome.lelievre9@gmail.com>
2017-03-07 12:58:02 +0100

    Creation du model membre et dump de la récupération de chaque membre inscrit

--- src/jerome/CoreBundle/Controller/CoreController.php
+++ src/jerome/CoreBundle/Controller/CoreController.php
@@ -2,12 +2,14 @@
 namespace src\jerome\CoreBundle\Controller;
 
 
+use jerome\CoreBundle\Model\Membres;
 use kayzore\bundle\KBundle\Controller;
 
 class CoreController extends Controller
 {
     public function homeAction() {
         echo $this->twig->render('front/accueil.html.twig', array(
+            'membres' => Membres::fetchAll()
         ));
     }
 }
---
+++ src/jerome/CoreBundle/Model/Membres.php
@@ -0,0 +1,209 @@
+<?php
+namespace jerome\CoreBundle\Model;
+
+
+use kayzore\bundle\KBundle\Cnx;
+
+class Membres
+{
+    private $id;
+    private $pseudo;
+    private $password;
+    private $nom;
+    private $prenom;
+    private $email;
+    private $civilite;
+    private $statut;
+
+    public function __construct(array $membre)
+    {
+        $this->setId($membre['id']);
+        $this->setPseudo($membre['pseudo']);
+        $this->setNom($membre['nom']);
+        $this->setPrenom($membre['prenom']);
+        $this->setEmail($membre['email']);
+        $this->setCivilite($membre['civilite']);
+        $this->setStatut($membre['statut']);
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * @param string $pseudo
+     * @return $this
+     */
+    public function setPseudo($pseudo)
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     * @return $this
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string $prenom
+     * @return $this
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return $this
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCivilite()
+    {
+        return $this->civilite;
+    }
+
+    /**
+     * @param string $civilite
+     * @return $this
+     */
+    public function setCivilite($civilite)
+    {
+        $this->civilite = $civilite;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatut()
+    {
+        return $this->statut;
+    }
+
+    /**
+     * @param int $statut
+     * @return $this
+     */
+    public function setStatut($statut)
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function connectAccount()
+    {
+
+    }
+
+    /**
+     * Récupère et retourne la liste de tout les membres au format objet
+     * @return array
+     */
+    public static function fetchAll()
+    {
+        $stmt = Cnx::getInstance()->query('SELECT * FROM membre');
+        $membres = [];
+
+        foreach ($stmt->fetchAll(\PDO::FETCH_ASSOC) as $membre) {
+            $membres[] = new self(array(
+                'id'        => $membre['id_membre'],
+                'pseudo'    => $membre['pseudo'],
+                'nom'       => $membre['nom'],
+                'prenom'    => $membre['prenom'],
+                'email'     => $membre['email'],
+                'civilite'  => $membre['civilite'],
+                'statut'    => $membre['statut']
+            ));
+        }
+
+        return $membres;
+    }
+}
