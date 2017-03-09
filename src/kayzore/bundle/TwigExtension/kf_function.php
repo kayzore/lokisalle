d34bb9cf1eb24f9a663c51fa0aa7898db37f6762
kayzore <jerome.lelievre2@gmail.com>
2017-03-09 14:09:23 +0100

    Creation d'un fonction twig qui verifie si l'utilisateur est connecté

--- src/kayzore/bundle/TwigExtension/kf_function.php
+++ src/kayzore/bundle/TwigExtension/kf_function.php
@@ -2,10 +2,15 @@
 $get_route_function = new Twig_SimpleFunction('getRoute', function ($name, array $options = []) {
     return $this->getUrl($name, $options);
 });
+$this->twig->addFunction($get_route_function);
 
 $get_assets = new Twig_SimpleFunction('assets', function ($path) {
     return 'http://' . $_SERVER['SERVER_NAME'] . '/' . \kayzore\bundle\KBundle\kFramework::getProjectSubFolder() . '/web/' . $path;
 });
-
-$this->twig->addFunction($get_route_function);
 $this->twig->addFunction($get_assets);
+
+$user_is_connected = new Twig_SimpleFunction('userIsConnected', function () {
+    return (isset($_SESSION[\kayzore\bundle\KBundle\kFramework::getProjectAlias(). '_utilisateur'])
+    && !is_null($_SESSION[\kayzore\bundle\KBundle\kFramework::getProjectAlias(). '_utilisateur']) ? true : false);
+});
+$this->twig->addFunction($user_is_connected);
