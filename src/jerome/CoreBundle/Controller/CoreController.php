c552500fd754c21977127599c0fa0480106d2b11
kayzore <jerome.lelievre2@gmail.com>
2017-03-10 09:25:58 +0100

    Page d'administration des produits Minor update sur le nom de la route de gestion des salles Creation de la route pour la gestion des produit Creation de la methode Modification de la page d'affichage

--- src/jerome/CoreBundle/Controller/CoreController.php
+++ src/jerome/CoreBundle/Controller/CoreController.php
@@ -140,4 +140,11 @@ class CoreController extends Controller
             'liste_salles' => Salle::fetchAll()
         ));
     }
+
+    public function adminProduitsAction()
+    {
+        echo $this->twig->render('back/produits.html.twig', array(
+            'liste_produits' => Produit::fetchAll()
+        ));
+    }
 }
